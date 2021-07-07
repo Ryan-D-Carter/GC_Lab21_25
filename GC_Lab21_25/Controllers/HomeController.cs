fba5dc7da017c2507aa0218820db3a1aab33adb2
Ryan <RyanCarterMI@gmail.com>
2021-07-07 16:44:07 -0400

    Initial Commit

---
+++ GC_Lab21_25/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GC_Lab21_25.Models;
+
+namespace GC_Lab21_25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult RegistrationPage()
+        {
+            return View(new Movie());
+        }
+
+        public IActionResult Results(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(movie);
+            }
+            else
+            {
+                return View("RegistrationPage", movie);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GC_Lab21_25/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GC_Lab21_25.Models
+{
+    public class Movie
+    {
+
+       
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage="Title can only be 50 characters max")]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+        
+        [Required]
+        [Range(typeof(DateTime), "1/1/1880", "12/31/2021", ErrorMessage = "Date must be between the year 1880 and 2021")]
+        public DateTime Year { get; set; }
+
+        public string Actors { get; set; }
+
+        public string Directors { get; set; }
+    }
+}
